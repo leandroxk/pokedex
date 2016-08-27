5287f27a9d1f7a5857e795b57dba3b118a495927
Leandro <leandroxk@gmail.com>
2016-08-27 09:39:13 -0300

    Melhorias WIP

--- alert.py
+++ alert.py
@@ -1,24 +1,47 @@
 import cherrypy
 import os
 
+from ntplib import NTPClient
+from retrying import retry
+
+
 class EmailAlert():
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
     def index(self):
-    	print Message(cherrypy.request.json).pokemon_id
+    	pokemon = Pokemon(cherrypy.request.json)
+    	print pokemon.id
+    	print pokemon.is_visible()
+    	print pokemon.remaining_secs()
 
-        return "Hello World!"
 
-
-class Message():
+class Pokemon():
 
 	def __init__(self, json):
 		body = json['message']
-		self.pokemon_id = body['pokemon_id']
+		self.id = body['pokemon_id']
 		self.longitude = body['longitude']
 		self.time_until_hidden = body['time_until_hidden_ms']
 		self.latitude = body['latitude']
+		self.disappear_time = body['disappear_time']
+
+	def is_visible(self):
+		return self._current_time() < self.disappear_time;
+
+	def remaining_secs(self):
+		remaining_s = self.disappear_time - self._current_time()
+		if remaining_s > 0:
+			return int(remaining_s)
+
+		return 0;
+
+	def get_map(self):
+		return ""
+
+	@retry(stop_max_delay=10000)
+	def _current_time(self):
+		return NTPClient().request('gps.ntp.br').tx_time
 
 
 config = {
