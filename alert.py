2ecae03638652918e735ef7b715459ea70a34690
Leandro <leandroxk@gmail.com>
2016-08-27 01:43:04 -0300

    initial commit

---
+++ alert.py
@@ -0,0 +1,30 @@
+import cherrypy
+
+
+class EmailAlert():
+
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    def index(self):
+    	print Message(cherrypy.request.json).pokemon_id
+
+        return "Hello World!"
+
+
+class Message():
+
+	def __init__(self, json):
+		body = json['message']
+		self.pokemon_id = body['pokemon_id']
+		self.longitude = body['longitude']
+		self.time_until_hidden = body['time_until_hidden_ms']
+		self.latitude = body['latitude']
+		
+
+config = {
+	'/': {
+        'tools.trailing_slash.on': False,
+        'tools.gzip.on': True
+    }
+}
+cherrypy.quickstart(EmailAlert(), '/alert', config)
